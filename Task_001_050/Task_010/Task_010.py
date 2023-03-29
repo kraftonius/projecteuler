e553aae64841f4c45573eae0f6e13c8e059f72ea
kraftonius <avd512@yandex.ru>
2023-03-29 23:42:55 +0300

    add Task_010

---
+++ Task_001_050/Task_010/Task_010.py
@@ -0,0 +1,19 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+
+def get_primes(n):
+    sieve = [True] * n
+    sieve[:2] = [False, False]
+    primes = []
+    for prime, is_prime in enumerate(sieve):
+        if not is_prime:
+            continue
+        primes.append(prime)
+        for not_prime in range(prime * prime, n, prime):
+            sieve[not_prime] = False
+    return primes
+
+
+number_max = 2 * 10 ** 6
+print(sum(get_primes(number_max)))
